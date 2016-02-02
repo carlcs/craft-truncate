5e27309dda6d9ade1b159423085ce37c00279997
carlcs <seelbach@gmx.de>
2016-02-02 14:47:18 +0100

    Reformatted code

--- truncate/TruncatePlugin.php
+++ truncate/TruncatePlugin.php
@@ -3,29 +3,29 @@ namespace Craft;
 
 class TruncatePlugin extends BasePlugin
 {
-	function getName()
-	{
-		return 'Truncate';
-	}
+    public function getName()
+    {
+        return 'Truncate';
+    }
 
-	function getVersion()
-	{
-		return '1.0';
-	}
+    public function getVersion()
+    {
+        return '1.0';
+    }
 
-	function getDeveloper()
-	{
-		return 'carlcs';
-	}
+    public function getDeveloper()
+    {
+        return 'carlcs';
+    }
 
-	function getDeveloperUrl()
-	{
-		return 'https://github.com/carlcs/craft-truncate';
-	}
+    public function getDeveloperUrl()
+    {
+        return 'https://github.com/carlcs/craft-truncate';
+    }
 
-	public function addTwigExtension()
-	{
-		Craft::import('plugins.truncate.twigextensions.TruncateTwigExtension');
-		return new TruncateTwigExtension();
-	}
+    public function addTwigExtension()
+    {
+        Craft::import('plugins.truncate.twigextensions.TruncateTwigExtension');
+        return new TruncateTwigExtension();
+    }
 }
--- truncate/twigextensions/TruncateTwigExtension.php
+++ truncate/twigextensions/TruncateTwigExtension.php
@@ -3,54 +3,51 @@ namespace Craft;
 
 class TruncateTwigExtension extends \Twig_Extension
 {
-	/**
-	 * Returns the name of the extension.
-	 *
-	 * @return string The extension name
-	 */
-	public function getName()
-	{
-		return 'Truncate';
-	}
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName()
+    {
+        return 'Truncate';
+    }
 
-	/**
-	 * Returns a list of filters to add to the existing list.
-	 *
-	 * @return array An array of filters
-	 */
-	public function getFilters()
-	{
-		return array(
-			new \Twig_SimpleFilter('truncate', array($this, 'truncateFilter')),
-		);
-	}
+    /**
+     * Returns a list of filters to add to the existing list.
+     *
+     * @return array An array of filters
+     */
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('truncate', array($this, 'truncateFilter')),
+        );
+    }
 
-	/**
-	 * Twig filter to cut off a string after limit is reached.
-	 *
-	 * @param string $value
-	 * @param number $length
-	 * @param boolean $preserve
-         * @param string $separator
-	 * @return string
-	 */
-        function truncateFilter($value, $length, $preserve = false, $separator = '...')
-        {
-                if (mb_strlen($value, 'utf8') > $length)
-                {
-                        if ($preserve)
-                        {
-                                if (false === ($breakpoint = mb_strpos($value, ' ', $length, 'utf8')))
-                                {
-                                        return $value;
-                                }
-
-                                $length = $breakpoint;
-                        }
-
-                        return rtrim(mb_substr($value, 0, $length, 'utf8')) . $separator;
+    /**
+     * Twig filter to cut off a string after limit is reached.
+     *
+     * @param string $value
+     * @param number $length
+     * @param boolean $preserve
+     * @param string $separator
+     * @return string
+     */
+    public function truncateFilter($value, $length, $preserve = false, $separator = '...')
+    {
+        if (mb_strlen($value, 'utf8') > $length) {
+            if ($preserve) {
+                if (false === ($breakpoint = mb_strpos($value, ' ', $length, 'utf8'))) {
+                    return $value;
                 }
 
-                return $value;
+                $length = $breakpoint;
+            }
+
+            return rtrim(mb_substr($value, 0, $length, 'utf8')) . $separator;
         }
+
+        return $value;
+    }
 }
